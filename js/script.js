86ffd22797f09002fea39d900598638afa63c414
Nirmean <ana_noona@live.com>
2024-01-16 22:28:31 +0000

    page style: styled header, search in put and start of today forecast

--- js/script.js
+++ js/script.js
@@ -31,5 +31,47 @@ async function getLocation(city) {
         lon: locationData[0].lon,
     };
 }
+
 // function to obtain current weather data based on city coordinates
 // function to obtain 5 day weather forecast
+//    - use coordinates from previous
+
+async function currentWeather(coordinates) {
+    let weatherLink = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${API_KEY}`;
+    let weatherResponse = await fetch(weatherLink);
+    let weatherData = await weatherResponse.json();
+    console.log(weatherData);
+
+    // display the current weather
+    let currentWeatherSection = $("#today");
+    currentWeatherSection.empty();
+
+    let cityName = $("<h2>").text(weatherData.name);
+    let date = $("<p>").text(dayjs().format("MMMM D, YYYY"));
+    let icon = $("<img>").attr("src", `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`);
+    let temperature = $("<p>").text(`Temperature: ${weatherData.main.temp} °C`);
+    let windSpeed = $("<p>").text(`Wind Speed: ${weatherData.wind.speed} m/s`);
+    let humidity = $("<p>").text(`Humidity: ${weatherData.main.humidity}%`);
+
+    currentWeatherSection.append(cityName, date, icon, temperature, windSpeed, humidity);
+
+    // Display 5-day forecast
+    
+    let forecastLink = `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude=current,minutely,hourly&appid=${API_KEY}`;
+    let forecastResponse = await fetch(forecastLink);
+    let forecastData = await forecastResponse.json();
+    console.log(forecastData);
+
+    let forecastSection = $("#forecast");
+    forecastSection.empty();
+
+    for (let i = 7; i <= 40; i+= 8) {
+        let forecastDate = dayjs.unix(forecastData.list[i].dt).format("MMMM D, YYYY");
+        let forecastIcon = $("<img>").attr("src", `https://openweathermap.org/img/wn/${forecastData.list[i].weather[0].icon}.png`);
+        let forecastTemperature = $("<p>").text(`Temperature: ${forecastData.list[i].main.temp} °C`);
+        let forecastHumidity = $("<p>").text(`Humidity: ${forecastData.list[i].main.humidity}%`);
+
+        let forecastDay = $("<div>").addClass("col-md-2 forecast-day").append(forecastDate, forecastIcon, forecastTemperature, forecastHumidity);
+        forecastSection.append(forecastDay);
+    }
+}
