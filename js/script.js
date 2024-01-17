a5756acb6328f10cdd0d2ed92d4b7d31d9129e51
Nirmean <ana_noona@live.com>
2024-01-17 16:40:41 +0000

    added function to display stored history to webpage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // API KEY variable
 const API_KEY = "63b28c6e37c471cac653534f235a28e0";
 
+const searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
 /*
 - "submit" function to initiate the api calls when the button is clicked
 - take value from input
@@ -11,12 +13,31 @@ $("#search-button").on("click", async function (event) {
 
     event.preventDefault();
     let cityInput = $("#search-input").val();
-    localStorage.setItem("name", cityInput);
+    searchHistory.push(cityInput);
+
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
 
     let latLon = await getLocation(cityInput);
     currentWeather(latLon);
 
+    displaySearchHistory();
+
+
 });
+
+// function to display the search history
+function displaySearchHistory() {
+    let searchHistorySection = $("#history");
+    searchHistorySection.empty();
+
+    let historyTitle = $("<h3>").text("Search History:");
+    searchHistorySection.append(historyTitle);
+
+    for (let i = 0; i < searchHistory.length; i++) {
+        let historyItem = $("<p>").text(searchHistory[i]).addClass("history-item");
+        searchHistorySection.append(historyItem);
+    }
+}
 
 
 // function to obtain city coordinates
@@ -56,7 +77,7 @@ async function currentWeather(coordinates) {
     currentWeatherSection.append(cityName, date, icon, temperature, windSpeed, humidity);
 
     // Display 5-day forecast
-    
+
     let forecastLink = `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude=current,minutely,hourly&appid=${API_KEY}`;
     let forecastResponse = await fetch(forecastLink);
     let forecastData = await forecastResponse.json();
@@ -66,14 +87,20 @@ async function currentWeather(coordinates) {
     forecastSection.empty();
 
 
-    for (let i = 7; i <= 40; i+= 8) {
-        let forecastDate = dayjs.unix(forecastData.list[i].dt).format("MMMM D, YYYY");
+    for (let i = 7; i <= 40; i += 8) {
+        let forecastDate = $("<p>").addClass("fDate").text(dayjs().format("MMMM D, YYYY"));
         let forecastIcon = $("<img>").attr("src", `https://openweathermap.org/img/wn/${forecastData.list[i].weather[0].icon}.png`).addClass("icon");
         let forecastTemperature = $("<p>").text(`Temperature: ${forecastData.list[i].main.temp} °C`);
         let forecastHumidity = $("<p>").text(`Humidity: ${forecastData.list[i].main.humidity}%`);
-        let forecastWind= $("<p>").text(`Wind Speed: ${forecastData.list[i].wind.speed} m/s`);
+        let forecastWind = $("<p>").text(`Wind Speed: ${forecastData.list[i].wind.speed} m/s`);
 
         let forecastDay = $("<div>").addClass("col-md-2 forecast-day").append(forecastDate, forecastIcon, forecastTemperature, forecastWind, forecastHumidity);
         forecastSection.append(forecastDay);
     }
 }
+
+// Display search history on page load
+$(document).ready(function () {
+    displaySearchHistory();
+});
+
